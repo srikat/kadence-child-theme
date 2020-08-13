c941638a411faa6f3e88d82e1b110c96882c7d4d
Sridhar Katakam <srikat@gmail.com>
2020-08-13 21:43:16 +1000

    Changed function name so there is no ambiguity.

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
  * Kadence child functions.
  */
 
-add_action( 'wp_enqueue_scripts', 'kadence_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'kadence_child_enqueue_styles' );
 /**
  * Enqueue current theme's stylesheet.
  */
-function kadence_enqueue_styles() {
+function kadence_child_enqueue_styles() {
 	wp_enqueue_style( 'kadence-child', get_stylesheet_directory_uri() . '/style.css', false, wp_get_theme()->get( 'Version' ) );
 }
 
